538c55a709ba05ac96e9d5927765202fc0855f12
Project 0rion <ezio.ac2@arcor.de>
2017-09-24 20:25:41 +0200

    add content to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 
 import SectionContainer from '../components/SectionContainer'
 import FooterSeparatorList from '../components/FooterSeparatorList'
@@ -8,16 +9,74 @@ import {footerBgColor, footerDarkerBgColor} from '../styles/colors'
 import {contentWidth} from '../styles/dimens'
 import strings from '../../data/strings'
 
+const Group = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: start;
+
+	&:not(:last-child) {
+		flex: 1 1 auto;
+	}
+
+	@media (max-width: 680px) {
+		margin: 2em 0;
+	}
+`
+
+const GroupHeader = styled.h3`
+	margin: 0;
+	margin-bottom: .5em;
+	color: ${footerDarkerBgColor};
+`
+
+const GroupItem = styled.div`
+	color: white;
+`
+
+const GroupLink = styled(Link)`
+	color: white;
+	border-bottom: 2px solid transparent;
+
+	&:hover {
+		border-color: white;
+	}
+`
+
 const Footer = () =>
 	<div>
 		<SectionContainer background={footerBgColor}>
 
-			<h1>Kontakt</h1>
-
 			<div
 				css={{
 					width: '100%',
+					display: 'flex',
+					flexDirection: 'row',
+
+					'@media (max-width: 680px)': {
+						flexDirection: 'column',
+					}
 				}}>
+
+				<Group>
+					<GroupHeader>Infos</GroupHeader>
+					<GroupLink to='/'>Startseite</GroupLink>
+					<GroupLink to='/service/'>{strings.serviceCaption}</GroupLink>
+					<GroupLink to='/contract/'>{strings.contractCaption}</GroupLink>
+					<GroupLink to='/contact/'>{strings.contactCaption}</GroupLink>
+				</Group>
+
+				<Group>
+					<GroupHeader>Wohnort</GroupHeader>
+					<GroupItem>Steffi Rühl</GroupItem>
+					<GroupItem>Gounodstraße 101</GroupItem>
+					<GroupItem>13088 Berlin</GroupItem>
+				</Group>
+
+				<Group>
+					<GroupHeader>Kontakt</GroupHeader>
+					<GroupItem>Tel.: 030 / 92 40 94 42</GroupItem>
+					<GroupItem>steffis-kruemelkiste@gmail.com</GroupItem>
+				</Group>
 			</div>
 		</SectionContainer>
 
